17b6ba6052c85c663ac0bac63285eb18cf8f037b
Cory <me@corylaflamme.com>
2019-06-12 23:20:43 -0400

    Added some homepage fanciness.

--- themes/portfolio/source/js/index.js
+++ themes/portfolio/source/js/index.js
@@ -1,3 +1,7 @@
 (function() {
   particlesJS.load('splash', 'particlesjs-config.json');
+
+  var sr = ScrollReveal();
+
+  sr.reveal('.reveal', { distance: '35px', duration: 500, interval: 200, viewFactor: 0.1 });
 })();
