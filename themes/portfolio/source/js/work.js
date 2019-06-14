22d3da21eb7bae909f992219b6272de4356ed5d7
Cory <me@corylaflamme.com>
2019-06-14 01:13:30 -0400

    Redid reveal animations for Work page.

--- themes/portfolio/source/js/work.js
+++ themes/portfolio/source/js/work.js
@@ -1,13 +1,11 @@
 (function() {
   var sr = ScrollReveal();
   var options = {
-    distance: '35px',
+    distance: '50px',
     duration: 1000,
     viewFactor: 0.1,
     interval: 200,
-    mobile: false,
   };
 
-  sr.reveal('.project-reveal-left', $.extend({}, options, { origin: 'left' }));
-  sr.reveal('.project-reveal-right', $.extend({}, options, { origin: 'right' }));
+  sr.reveal('.reveal', $.extend({}, options));
 })();
